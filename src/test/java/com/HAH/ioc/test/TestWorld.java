68ad4fcc6bcb18191c0260688046732ee9aa32f7
hhtun137 <htunaunghlaing199@gmail.com>
2024-02-27 23:40:49 +0630

    filesystem

--- src/test/java/com/HAH/ioc/test/TestWorld.java
+++ src/test/java/com/HAH/ioc/test/TestWorld.java
@@ -1,19 +1,20 @@
 package com.HAH.ioc.test;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.HAH.ioc.HelloWorld;
 
 public class TestWorld {
 	
-	// This is ClassPathXml Configuration
-	// ClassPathXml Configuration support multiple refresh
-		@Test
-		public void demoTest() {
-			var context = new ClassPathXmlApplicationContext("classpath:context.xml");
-			context.refresh(); 
-			var bean = context.getBean(HelloWorld.class);
-			bean.getWorld();
-		}
+//	This is file system Xml Configuration
+//	If you wanna this config, your context.xml file need to enter the file system of project folder.
+//	(If context.xml file having in resources, delete or change location)
+	@Test
+	public void demoTest() {
+		var context = new FileSystemXmlApplicationContext("context.xml");
+		context.refresh(); 
+		var bean = context.getBean(HelloWorld.class);
+		bean.getWorld();
+	}
 }
