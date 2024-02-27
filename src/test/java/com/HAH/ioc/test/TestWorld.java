ed3806ca779a0b481dafc8bdabee09f3c30ecf56
hhtun137 <htunaunghlaing199@gmail.com>
2024-02-27 22:23:52 +0630

    classpath

--- src/test/java/com/HAH/ioc/test/TestWorld.java
+++ src/test/java/com/HAH/ioc/test/TestWorld.java
@@ -1,27 +1,19 @@
 package com.HAH.ioc.test;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.HAH.ioc.HelloWorld;
 
 public class TestWorld {
 	
-//  This is GenericXml Configuration
-//  GenericXml Configuration does not support multiple refresh
-//	You can see two types(Argument with classpath and default) of GenericXml Configuration.
-//	@Test
-//	public void demoTest() {
-//		var context = new GenericXmlApplicationContext("classpath:context.xml");
-//			context.load("classpath:context.xml");
-//			context.refresh();
-//		var bean = context.getBean(HelloWorld.class);
-//		bean.getWorld();
-//	}
-	@Test
-	public void demoTest() {
-		var context = new GenericXmlApplicationContext("classpath:context.xml");
-		var bean = context.getBean(HelloWorld.class);
-		bean.getWorld();
-	}
+	// This is ClassPathXml Configuration
+	// ClassPathXml Configuration support multiple refresh
+		@Test
+		public void demoTest() {
+			var context = new ClassPathXmlApplicationContext("classpath:context.xml");
+			context.refresh(); 
+			var bean = context.getBean(HelloWorld.class);
+			bean.getWorld();
+		}
 }
